dcd6c95a9b04ed2f66ede783317b274138714f9b
Harsh Rathore <harshrathore1530@gmail.com>
2026-08-18 19:55:35 +0530

    Create Home component for Vectolift Australia

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="p-8">
+      <h1 className="text-4xl font-bold">Vectolift Australia</h1>
+      <p className="mt-4">Professional Lifting Solutions</p>
+    </main>
+  )
+}
