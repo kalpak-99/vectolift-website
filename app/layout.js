b6730752f578ce2e337a8569fc4f95c6cfcffe6f
Harsh Rathore <harshrathore1530@gmail.com>
2026-08-18 20:22:46 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Vectolift Australia',
+  description: 'Professional Lifting Solutions',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
