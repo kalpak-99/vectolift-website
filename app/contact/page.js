9573d56309d0c5c99fc55a0607fa00ef6eb04ab4
Harsh Rathore <harshrathore1530@gmail.com>
2026-08-18 20:06:01 +0530

    Create page.js

---
+++ app/contact/page.js
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold">Contact Us</h1>
+      <p className="mt-4">Email: info@vectolift.com.au</p>
+      <p>Phone: +61 123 456 789</p>
+    </div>
+  )
+}
