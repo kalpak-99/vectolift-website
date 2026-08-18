6c18d0d2a1fcd793d61170b986a8299133964ede
Harsh Rathore <harshrathore1530@gmail.com>
2026-08-18 20:31:17 +0530

    Delete app/about directory

--- app/about/page.js
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold">About Vectolift</h1>
-      <p className="mt-4">We provide professional lifting solutions across Australia.</p>
-    </div>
-  )
-}
